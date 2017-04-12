21933468dc31ab47ce8a34c28dab059179fb3a1d
refgjin <refgjin@gmail.com>
2017-04-13 03:09:54 +0900

    Upload bootstrap & Display Index

--- router/main.js
+++ router/main.js
@@ -3,7 +3,7 @@ module.exports = function(app)
      app.get('/',function(req,res){
         res.render('index.html')
      });
-     app.get('/about',function(req,res){
-        res.render('about.html');
+     app.get('/pages/charts/chartjs.html',function(req,res){
+        res.render('pages/charts/chartjs.html');
     });
 }
