19030147ff2fec1e17c9b943028bb9bc4a61944e
refgjin <refgjin@gmail.com>
2017-04-19 04:22:22 +0900

    Transfer data to index.html (ages, gender, registers)

--- router/main.js
+++ router/main.js
@@ -1,16 +1,32 @@
+var mysql = require('mysql');
+var dbconfig = require(__dirname+'/../config/db-config.json');
+var connection = mysql.createConnection(dbconfig);
+
 module.exports = function(app)
 {
-	app.get('/',(req,res)=>{
+	/*app.get('/',function(req,res){
 		connection.query('select admin_id,admin_name from ADMINACCOUNT',(err, rows)=>{
 			if(err) throw err;
 
 			res.send(rows);
-		})
+		});
+	});*/
+	app.get('/',function(req,res){
+		var total_register = 15;
+		var yesterday_register = 3;
+
+		res.render('index.html',{
+			total_register : total_register,
+			yesterday_register : yesterday_register,
+			male : 20,
+			female : 25,
+			age_10 : 10,
+			age_20 : 30,
+			age_30 : 30,
+			age_40 : 15
+		});
 	});
-     /*app.get('/',function(req,res){
-        res.render('index.html')
-     });*/
-     app.get('/pages/charts/chartjs.html',function(req,res){
-        res.render('pages/charts/chartjs.html');
+	app.get('/pages/charts/chartjs.html',function(req,res){
+		res.render('pages/charts/chartjs.html');
     });
 }
--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-import mysql from 'mysql';
-import express from 'express';
+var express = require('express');
 
-let dbconfig = require(__dirname+'/config/db-config.json');
-let connection = mysql.createConnection(dbconfig);
 
 const app = express();
 const port = 3333;
 var router = require('./router/main')(app);
  
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
  
-var server = app.listen(3333, function(){
-    console.log("Express server has started on port 3000")
+var server = app.listen(port, function(){
+    console.log("Express server has started on port 3333")
 });
 
 app.use(express.static('public'));
