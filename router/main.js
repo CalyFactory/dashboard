0d508fe6cd1db057b6a16753bff315fc93e18314
refgjin <refgjin@gmail.com>
2017-04-19 03:29:42 +0900

    commit for upload to server

--- router/main.js
+++ router/main.js
@@ -1,8 +1,15 @@
 module.exports = function(app)
 {
-     app.get('/',function(req,res){
+	app.get('/',(req,res)=>{
+		connection.query('select admin_id,admin_name from ADMINACCOUNT',(err, rows)=>{
+			if(err) throw err;
+
+			res.send(rows);
+		})
+	});
+     /*app.get('/',function(req,res){
         res.render('index.html')
-     });
+     });*/
      app.get('/pages/charts/chartjs.html',function(req,res){
         res.render('pages/charts/chartjs.html');
     });
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-var express = require('express');
-var app = express();
+import mysql from 'mysql';
+import express from 'express';
+
+let dbconfig = require(__dirname+'/config/db-config.json');
+let connection = mysql.createConnection(dbconfig);
+
+const app = express();
+const port = 3333;
 var router = require('./router/main')(app);
  
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
  
-var server = app.listen(3000, function(){
+var server = app.listen(3333, function(){
     console.log("Express server has started on port 3000")
 });
 
